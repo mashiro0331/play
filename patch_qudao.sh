c8f7f6e002a15e41d1054bb7b7aec7c27db430cb
mashiro0331 <128968158+mashiro0331@users.noreply.github.com>
2023-03-26 18:24:28 +0800

    Update patch_qudao.sh

--- patch_qudao.sh
+++ patch_qudao.sh
@@ -6,7 +6,7 @@
 download_azurlane () {
     if [ ! -f "AzurLane.apk" ]; then
     # 这个链接是MUMU下载的,应该是9游,其他渠道自行修改直链
-    url="https://apkmobilecdn1-v6dl.vivo.com.cn/appstore/developer/apk/20221114/202211141250028xqco.apk"
+    url="https://gameapkwrdl.vivo.com.cn/appstore/developer/apk/20221114/202211141250028xqco.apk"
     #这个链接是当乐网
     #url="https://res5.d.cn/4fea10ef16b7b0d5ea0f3a5ad9d3483753ab0106c1a0cdf1d67434fec122fbca9819ee365b07dae4ccbf47d02992f9785f5cc48955b4765f919e8a2ac789488082c80bca10f6617e.apk"
     # 使用curl命令下载apk文件
